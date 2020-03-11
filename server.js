43c65cfc086494934749f678af144070b04cbb60
lihuang-zheng <jerryzheng0131@gmail.com>
2020-03-11 15:19:34 -0500

    uncomment the default route.

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ server.use(logger);
 server.use("/api/users", userRouter);
 server.use("/api/posts", postRouter);
 
-// server.get("/", (req, res) => {
-//   res.send(`<h2>Let's write some middleware!</h2>`);
-// });
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
+});
 
 //custom middleware
 
