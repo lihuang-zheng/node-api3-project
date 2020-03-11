ceba0811a8d955c70ce423ab57c22d2fc616d227
lihuang-zheng <jerryzheng0131@gmail.com>
2020-03-11 15:11:05 -0500

    fixed the server issue and it now working.

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require("./server");
 const PORT = 5000;
 
 server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`);
 });
--- server.js
+++ server.js
@@ -10,16 +10,16 @@ server.use(logger);
 server.use("/api/users", userRouter);
 server.use("/api/posts", postRouter);
 
-server.get("/", (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
+// server.get("/", (req, res) => {
+//   res.send(`<h2>Let's write some middleware!</h2>`);
+// });
 
 //custom middleware
 
 function logger(req, res, next) {
   console.log(
-    `${req.method} from ${req.get("Origin")} at ${new Date().toISOString}`
+    `${req.method} from ${req.get("Origin")} at ${new Date().toISOString()}`
   );
+  next();
 }
-
 module.exports = server;
